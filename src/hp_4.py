ef6e6e4e5a8ec565fe4f50e0b52674a3a9a573f1
janakiramsabbineni <vsabbine@cbu.edu>
2024-02-24 11:02:27 -0600

    Update hp_4.py
    completed

--- src/hp_4.py
+++ src/hp_4.py
@@ -5,19 +5,25 @@ from csv import DictReader, DictWriter
 from collections import defaultdict
 
 def reformat_dates(old_dates):
-
+    """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
+    element to a format dd mmm yyyy--01 Jan 2001."""
     pass  # Implement this function
 
 def date_range(start, n):
-
+    """For input date string `start`, with format 'yyyy-mm-dd', returns
+    a list of `n` datetime objects starting at `start` where each
+    element in the list is one day after the previous."""
     pass  # Implement this function
 
 def add_date_range(values, start_date):
-
+    """Adds a daily date range to the list `values` beginning with
+    `start_date`.  The date, value pairs are returned as tuples
+    in the returned list."""
     pass  # Implement this function
 
 def fees_report(infile, outfile):
-
+    """Calculates late fees per patron id and writes a summary report to
+    outfile."""
     pass  # Implement this function
 
 # The following main selection block will only run when you choose
@@ -25,4 +31,3 @@ def fees_report(infile, outfile):
 if __name__ == '__main__':
     # You can add test code here to check the functionality of your functions
     pass
-
